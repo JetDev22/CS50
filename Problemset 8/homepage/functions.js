c43b7e19e0bbfbafac4115a499b45988e3c473c7
JetDev22 <thomas.roelig@gmail.com>
2023-03-11 16:35:46 +0100

    started cs50 homepage problem

---
+++ Problemset 8/homepage/functions.js
@@ -0,0 +1,17 @@
+// functions for to-do list
+
+function submitInput(){
+    // create table row from input
+}
+
+function clearInput(){
+    // clear all input fields
+}
+
+function showActive(){
+    // show table with active todos
+}
+
+function showDone(){
+    // show table with completed todos
+}
